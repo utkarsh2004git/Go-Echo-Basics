17cdbea69ba005b12e405afc0d9e7a60d014a613
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-03-30 13:28:58 +0530

    add customContext

--- main.go
+++ main.go
@@ -7,6 +7,17 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type CustomContext struct {
+	echo.Context
+}
+
+func (c *CustomContext) Foo() {
+	println("foo")
+}
+func (c *CustomContext) Bar() {
+	println("bar")
+}
+
 func HelloWorld(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World")
 }
@@ -25,19 +36,22 @@ func QueryEcho(c echo.Context) error {
 type User struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
-
 }
 
-func UserRegister(c echo.Context) error{
+func UserRegister(c echo.Context) error {
 	var u User
-	err := c.Bind(&u);
-	if(err!=nil){
+	err := c.Bind(&u)
+	if err != nil {
 		return err
 	}
+	
+	cc := c.(*CustomContext)
+	cc.Foo()
+	cc.Bar()
 
 	fmt.Println(u)
 
-	return c.JSON(http.StatusOK,u)
+	return c.JSON(http.StatusOK, u)
 }
 
 func main() {
@@ -47,7 +61,15 @@ func main() {
 	e.GET("/:search", ParamsEcho)
 	e.GET("/query", QueryEcho)
 
-	e.POST("/user",UserRegister)
+	//customContext applied from here
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &CustomContext{c}
+			return next(cc)
+		}
+	})
+
+	e.POST("/user", UserRegister)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
